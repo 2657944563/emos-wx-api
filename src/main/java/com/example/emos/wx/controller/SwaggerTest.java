05f9831dfb502955b3ee9074835dbdf7ec2ea34c
呙启伟 <2657944563@qq.com>
2022-06-30 00:31:04 +0800

    feat(new): 添加了Validation作为参数验证

--- src/main/java/com/example/emos/wx/controller/SwaggerTest.java
+++ src/main/java/com/example/emos/wx/controller/SwaggerTest.java
@@ -1,11 +1,12 @@
 package com.example.emos.wx.controller;
 
 import com.example.emos.wx.common.util.R;
+import com.example.emos.wx.controller.from.SwaggerTestFrom;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/test")
@@ -16,5 +17,11 @@ public class SwaggerTest {
     @ApiOperation("Swagger测试方法")
     public R swagger() {
         return R.ok().put("msg", "hello swagger");
+    }
+
+    @PostMapping("sayHello")
+    @ApiOperation("Swagger测试方法")
+    public R swagger(@Valid @RequestBody SwaggerTestFrom from) {
+        return R.ok().put("msg", "hello swagger").put("name", from.getName());
     }
 }
